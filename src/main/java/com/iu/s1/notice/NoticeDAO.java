77dbe7917997a0d9c8e83d3623b38efc1635a78a
jerry <woffl_@naver.com>
2022-03-07 09:50:26 +0900

    change

--- src/main/java/com/iu/s1/notice/NoticeDAO.java
+++ src/main/java/com/iu/s1/notice/NoticeDAO.java
@@ -10,22 +10,22 @@ import org.springframework.stereotype.Repository;
 public class NoticeDAO {
 	@Autowired
 	private SqlSession sqlSession;
-	private final String NAMASPACE = "com.iu.s1.notice.NoticeDAO.";
+	private final String NAMESPACE = "com.iu.s1.notice.NoticeDAO.";
 	
 	public List<NoticeDTO> list() throws Exception{
-		return sqlSession.selectList(NAMASPACE+"list");
+		return sqlSession.selectList(NAMESPACE+"list");
 	}
 	
 	public NoticeDTO detail(NoticeDTO noticeDTO) throws Exception{
 		
-		return sqlSession.selectOne(NAMASPACE+"detail", noticeDTO);
+		return sqlSession.selectOne(NAMESPACE+"detail", noticeDTO);
 	}
 	
 	public int add(NoticeDTO noticeDTO) throws Exception{
-		return sqlSession.insert(NAMASPACE+"add", noticeDTO);
+		return sqlSession.insert(NAMESPACE+"add", noticeDTO);
 	}
 	
 	public int delete(NoticeDTO noticeDTO) throws Exception{
-		return sqlSession.delete(NAMASPACE+"delete",noticeDTO);
+		return sqlSession.delete(NAMESPACE+"delete",noticeDTO);
 	}
 }
